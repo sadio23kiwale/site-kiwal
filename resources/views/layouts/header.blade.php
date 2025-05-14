0d762784c0f19ca9427f46d09a6827fa5df4896b
sadio23kiwale <sadjobah@kiwale.com>
2025-05-14 17:15:49 +0000

    Quelques modifications

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -16,19 +16,9 @@
                     <li class="nav-item">
                         <a class="nav-link" href="{{ url('service') }}">Service</a>
                     </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="{{ url('contact') }}" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                            Contact
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('contact') }}">Contact</a>
+                    </li>
                     </li>
                 </ul>
                 <form class="d-flex" role="search">
